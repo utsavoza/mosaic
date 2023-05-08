c2a56c64815525767eac60d5ec3c08b65e42e37b
Utsav Oza <utsavoza96@gmail.com>
2023-05-08 18:12:36 -0400

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Mosaic
+<div align="center">
+
+# <b>Mosaic</b>
+
+### Generating The New Yorker Style Cartoons using Text-to-Image Diffusion Models
+
+</div>
---
+++ dataset.py
@@ -0,0 +1,9 @@
+class DatasetUtils:
+    def __init__(
+        self,
+        dataset_archive: str = None,
+        batch_size: int = 4,
+        img_height: int = 256,
+        img_width: int = 256
+    ) -> None:
+        pass
